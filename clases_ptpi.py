0197d582f6ae407e467246e97ef794bb39602283
Elu <elurisoto@gmail.com>
2014-04-01 13:43:03 +0200

    Sprites corregidos y fondo estático añadido

--- clases_ptpi.py
+++ clases_ptpi.py
@@ -14,8 +14,12 @@ alto_pantalla = 600
 
 
 screen = pygame.display.set_mode((ancho_pantalla, alto_pantalla))
+#screen = pygame.Surface((ancho_pantalla, alto_pantalla), pygame.SRCALPHA)
+#screen.set_colorkey((0,67,171))
+screen.set_alpha()
+
 ss = spritesheet("1945.png")
-sprites = pygame.sprite.Group()
+sprites = pygame.sprite.LayeredUpdates()
 
 class Jugador(pygame.sprite.Sprite):
 	x = 170
@@ -23,33 +27,35 @@ class Jugador(pygame.sprite.Sprite):
 	ancho = 60
 	alto = 60
 	color = (68,184,172)
+	radius = 30
+	layer = 0
 
 	def __init__(self):
 		self.rect = pygame.Rect(self.x, self.y, self.ancho, self.alto)
 		pygame.sprite.Sprite.__init__(self)
-		self.image = ss.image_at((220,270,205,30))
+		self.image = ss.image_at((301,235,65,65),(0,67,171))
 		sprites.add(self)
 
 
 	def update(self, pressed):
 		if pressed[pygame.K_LEFT]: 
 			if self.rect.x > 0:
-				self.rect.x -= 3
+				self.rect.move_ip(-3, 0)
 		if pressed[pygame.K_RIGHT]: 
 			if self.rect.x < ancho_pantalla - self.ancho:
-				self.rect.x += 3
+				self.rect.move_ip(3, 0)
 
 
-	def dibuja(self):
-		sprites.draw(screen)
 class Enemigo(pygame.sprite.Sprite):
 
 	x = 0
 	y = 0
-	ancho = 10
-	alto = 10
+	ancho = 30
+	alto = 30
 	velocidad = 1
 	color = ROJO
+	radius = 10
+	layer = 0
 
 	def __init__(self, x, y, alto, ancho, velocidad, color):
 		self.x = x
@@ -58,10 +64,11 @@ class Enemigo(pygame.sprite.Sprite):
 		self.color = color
 		self.alto = alto
 		self.ancho = ancho
-		self.rect = pygame.Rect(x, y, ancho, alto)
+		self.rect = pygame.Rect(self.x, self.y, self.ancho, self.alto)
+		pygame.sprite.Sprite.__init__(self)
+		self.image = ss.image_at((5,5,30,30),(0,67,171))
+		sprites.add(self)
 
-	def movimiento(self):
+	def update(self):
 		self.rect.y += self.velocidad
 
-	def dibuja(self):
-		pygame.draw.rect(screen, self.color, self.rect)
--- ptpi.py
+++ ptpi.py
@@ -1,10 +1,27 @@
 from clases_ptpi import *
-from spritesheet import *
+from ssheet import *
+from sstripanim import *
+from copy import copy
 
 def colision_enemigos(enemigos, jugador):
 	for i, enem in enumerate (enemigos):
-		if enem.rect.colliderect(jugador):
+		if pygame.sprite.collide_circle(enemigos[i], jugador):
+			enemigos[i].kill()
 			del enemigos[i]
+
+def dibuja_fondo():
+
+	fondo.image = ss.image_at((268,367,32,32))
+	# fondo.rect = pygame.Rect((0,0,32,32))
+	fondo.layer = 2
+	# sprites.add(copy(fondo))
+
+	for i in range(0,ancho_pantalla,32):
+		for j in range(0,alto_pantalla,32):
+
+			fondo.rect = pygame.Rect((i,j,32,32))
+			sprites.add(copy(fondo)) 		#No me convence
+
 
 
 pygame.init()
@@ -14,6 +31,10 @@ enemigos = []
 
 clock = pygame.time.Clock()
 t_enemigo = pygame.time.get_ticks()
+fondo = pygame.sprite.Sprite()
+
+dibuja_fondo()
+
 jugador = Jugador()
 
 
@@ -26,23 +47,22 @@ while not done:
 
 		if pressed[pygame.K_ESCAPE]: done = True
 		
-		screen.fill((0, 67, 171))
-		
+		#screen.fill((0, 67, 171))
+		screen.fill((0,0,0,0))
 		jugador.update(pressed)
-		jugador.dibuja()
+		sprites.draw(screen)
 
 		if pygame.time.get_ticks() - t_enemigo > 750:
-			e_x = random.randint(5, 395)
+			e_x = random.randint(5, 370)
 			e_y = -10
-			enemigos.append(Enemigo(e_x,e_y, 10, 10, 3, ROJO))
+			enemigos.append(Enemigo(e_x,e_y, 30, 30, 3, ROJO))
 			t_enemigo = pygame.time.get_ticks()
 
 
 		colision_enemigos(enemigos, jugador)
 
 		for e in enemigos:
-			e.movimiento()
-			e.dibuja()
+			e.update()
 
 
 
